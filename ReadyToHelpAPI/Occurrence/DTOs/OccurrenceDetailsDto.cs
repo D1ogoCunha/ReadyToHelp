324e2a5d6ee9e4d1888f60f37daae56ea952868c
rnnsousa <rafannsousa@gmail.com>
2025-10-24 02:39:27 +0100

    feature(occurrence): Added GET occurrence details endpoint and the respective tests

---
+++ ReadyToHelpAPI/Occurrence/DTOs/OccurrenceDetailsDto.cs
@@ -0,0 +1,17 @@
+namespace readytohelpapi.Occurrence.Models;
+
+public class OccurrenceDetailsDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public OccurrenceType Type { get; set; }
+    public OccurrenceStatus Status { get; set; }
+    public PriorityLevel Priority { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public DateTime CreationDateTime { get; set; }
+    public DateTime? EndDateTime { get; set; }
+    public int? ResponsibleEntityId { get; set; }
+    public int ReportCount { get; set; }
+}
