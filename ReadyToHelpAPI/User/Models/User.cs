5ada296b089efe33977f2118427c51bd5ec70393
Tomás Nunes <8220107@estg.ipp.pt>
2025-11-06 14:44:47 +0000

    refactor(gitlab-ci.yml): optimized pipeline v2

--- ReadyToHelpAPI/User/Models/User.cs
+++ ReadyToHelpAPI/User/Models/User.cs
@@ -1,8 +1,4 @@
-﻿// <copyright file="User.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-namespace readytohelpapi.User.Models;
+﻿namespace readytohelpapi.User.Models;
 
 /// <summary>
 ///     Represents a user.
