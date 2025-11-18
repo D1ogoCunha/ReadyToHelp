8a81d21f8ccc52d69703ef788295113d6df0ee1d
Diogo Cunha <8220100@estg.ipp.pt>
2025-11-18 19:40:03 +0000

    feature(map-mobile): added map using mapbox api

--- MOBILE/app/build.gradle.kts
+++ MOBILE/app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -16,7 +17,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -50,6 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.play.services.maps)
+    implementation("com.mapbox.maps:android-ndk27:11.16.4")
+    implementation("com.mapbox.extension:maps-compose:11.16.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
